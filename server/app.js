962abb5bbd1ae80dda220ba791932886c49f9816
David Gimenez <alejandrogmnz@gmail.com>
2022-01-12 14:39:02 -0400

    .

--- server/app.js
+++ server/app.js
@@ -1,30 +1,121 @@
 const fetch = require('cross-fetch');
+const algosdk = require('algosdk');
+const dataParse = JSON.parse(fs.readFileSync('wallet_creator.json').toString());
 
-setInterval(() => {
-    console.clear();
-    let urlSite =
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'; // URL de la API
+const wallet_creator = algosdk.mnemonicToSecretKey(dataParse.mnemonic);
 
-    // Obtencion de la data desde la API por un endpoint
+const token = {
+    'X-API-Key': '79vc1nPNxj50rxauKFEI35oynac24jzo3EE6tYZc',
+};
+const server = 'https://testnet-algorand.api.purestake.io/ps2';
+const port = '';
+const algoClient = new algosdk.Algodv2(token, server, port);
+const coingeckoApi =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=75&page=1&sparkline=false'; // URL de la API
 
-    fetch(urlSite)
-        .then((response) => response.json())
-        .then((data) => mostrarData(data))
-        .catch((e) => console.log(e));
+const update_slot_1 = [
+    'BTC',
+    'ETH',
+    'BNB',
+    'SOL',
+    'ADA',
+    'XRP',
+    'LUNA',
+    'DOT',
+    'AVAX',
+    'DOGE',
+    'SHIB',
+    'MATIC',
+    'CRO',
+    'WBTC',
+    'UNI',
+];
 
-    // Funcion encargada de mostrar y procesar la data
+const update_slot_2 = [
+    'LTC',
+    'LINK',
+    'ALGO',
+    'BCH',
+    'NEAR',
+    'TRX',
+    'XML',
+    'MANA',
+    'AXS',
+    'ATOM',
+    'VET',
+    'FTT',
+    'FTM',
+    'SAND',
+    'FIL',
+];
 
-    const mostrarData = (data) => {
-        let arrCrypto = [];
+const update_slot_3 = [
+    'HBAR',
+    'BTCB',
+    'THETA',
+    'EGLD',
+    'ICP',
+    'ETC',
+    'MIOTA',
+    'XTZ',
+    'HNT',
+    'XMR',
+    'AAVE',
+    'LEO',
+    'KLAY',
+    'GALA',
+    'GRT',
+];
 
-        for (let i = 0; i < data.length; i++) {
-            const symbol = data[i].symbol;
-            const price = data[i].current_price;
-            arrCrypto.push(symbol + ' | ' + price);
-        }
+const update_slot_4 = [
+    'EOS',
+    'CAKE',
+    'STX',
+    'FLOW',
+    'LRC',
+    'ONE',
+    'BTT',
+    'KSM',
+    'MKR',
+    'ENJ',
+    'BSV',
+    'QNT',
+    'AMP',
+    'KDA',
+    'XEC',
+];
 
-        for (let i = 0; i < arrCrypto.length; i++) {
-            console.log(arrCrypto[i]);
-        }
-    };
-}, 4000); // Tiempo que se tarda en actualizar el server
+const main = async () => {
+    setInterval(() => {
+        const params = await algoClient.getTransactionParams().do();
+
+        // Get data from coingecko's API
+        fetch(coingeckoApi)
+            .then((response) => response.json())
+            .then((data) => mostrarData(data))
+            .catch((e) => console.log(e));
+
+        // Funcion encargada de mostrar y procesar la data
+        const mostrarData = (data) => {
+            let arrCrypto = [];
+
+            for (let i = 0; i < data.length; i++) {
+                const symbol = data[i].symbol;
+                const price = data[i].current_price;
+                arrCrypto.push(symbol + ' | ' + price);
+            }
+
+            for (let i = 0; i < arrCrypto.length; i++) {
+                console.log(arrCrypto[i]);
+            }
+        };
+        const txn1 = algosdk.makeApplicationNoOpTxn(
+            wallet_creator.addr,
+            params,
+            182182812,
+            [],
+        );
+    }, 4000); // Tiempo que se tarda en actualizar el server
+};
+
+main();
