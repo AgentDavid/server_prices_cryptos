bda858f88a0afb7e50dae25fbb65c18e63840eb2
David Gimenez <alejandrogmnz@gmail.com>
2022-01-13 21:32:43 -0400

    .

--- server/app.js
+++ server/app.js
@@ -1,12 +1,15 @@
 const fetch = require('cross-fetch');
-const algosdk = require('algosdk');
-const dataParse = JSON.parse(fs.readFileSync('wallet_creator.json').toString());
+// const algosdk = require('algosdk');
+// const fs = require('fs');
 
-const wallet_creator = algosdk.mnemonicToSecretKey(dataParse.mnemonic);
+// const dataParse = JSON.parse(fs.readFileSync('wallet_creator.json').toString());
 
-const server = 'https://testnet.algoexplorerapi.io';
-const algoClient = new algosdk.Algodv2('', server, '');
-const coingeckoApi = ('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana,cardano,rippler,terra-luna,polkadot,avalanche-2,dogecoin,shiba-inu,matic-network,crypto-com-chain,wrapped-bitcoin,uniswap,litecoin,chainlink,algorand,bitcoin-cash,near,tron,stellar,decentraland,axie-infinity,cosmos,vechain,ftx-token,fantom,the-sandbox,filecoin,hedera-hashgraph,bitcoin-bep2,theta-token,elrond-erd-2,internet-computer,ethereum-claiota,tezos,helium,monero,aave,leo-token,klay-token,gala,the-graph,eos,pancakeswap-token,blockstack,flow,loopring,harmony,bittorrent-2,kusama,maker,enjincoin,bitcoin-cash-sv,quant-network,amp-token,kadena,ecash&vs_currencies=usd');
+// const wallet_creator = algosdk.mnemonicToSecretKey(dataParse.mnemonic);
+
+// const server = 'https://testnet.algoexplorerapi.io';
+// const algoClient = new algosdk.Algodv2('', server, '');
+const coingeckoApi =
+    'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,binancecoin,solana,cardano,rippler,terra-luna,polkadot,avalanche-2,dogecoin,shiba-inu,matic-network,crypto-com-chain,wrapped-bitcoin,uniswap,litecoin,chainlink,algorand,bitcoin-cash,near,tron,stellar,decentraland,axie-infinity,cosmos,vechain,ftx-token,fantom,the-sandbox,filecoin,hedera-hashgraph,bitcoin-bep2,theta-token,elrond-erd-2,internet-computer,ethereum-claiota,tezos,helium,monero,aave,leo-token,klay-token,gala,the-graph,eos,pancakeswap-token,blockstack,flow,loopring,harmony,bittorrent-2,kusama,maker,enjincoin,bitcoin-cash-sv,quant-network,amp-token,kadena,ecash&vs_currencies=usd';
 
 const update_slot_1 = [
     'bitcoin', //btc
@@ -82,7 +85,7 @@ const update_slot_4 = [
 
 const main = async () => {
     setInterval(() => {
-        const params = await algoClient.getTransactionParams().do();
+        // const params = await algoClient.getTransactionParams().do();
 
         // Get data from coingecko's API
         fetch(coingeckoApi)
@@ -90,88 +93,84 @@ const main = async () => {
             .then((data) => getData(data))
             .catch((e) => console.log(e));
 
-const getData = (data) => {
+        const getData = (data) => {
+            console.log(data);
+            const var1 = [
+                new Uint8Array(Buffer.from('update_slot_1')),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[0]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[1]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[2]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[3]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[4]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[5]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[6]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[7]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[8]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[9]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[10]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[11]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[12]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[13]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[14]].usd * 1000000))),
+            ];
+            const var2 = [
+                new Uint8Array(Buffer.from('update_slot_2')),
 
-    [
-        new Uint8Array(Buffer.from('update_slot_1')),
-        
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[0]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[1]].usd * 1000000))), 
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[2]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[3]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[4]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[5]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[6]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[7]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[8]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[9]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[10]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[11]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[12]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[13]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_1[14]].usd * 1000000)))
-    ],
-    [
-        new Uint8Array(Buffer.from('update_slot_2')),
-    
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[0]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[1]].usd * 1000000))), 
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[2]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[3]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[4]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[5]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[6]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[7]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[8]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[9]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[10]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[11]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[12]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[13]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[14]].usd * 1000000)))
-    ],
-    [
-        new Uint8Array(Buffer.from('update_slot_3')),
-        
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[0]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[1]].usd * 1000000))), 
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[2]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[3]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[4]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[5]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[6]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[7]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[8]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[9]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[10]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[11]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[12]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[13]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[14]].usd * 1000000)))
-    ],
-    [
-        new Uint8Array(Buffer.from('update_slot_4')),
-        
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[0]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[1]].usd * 1000000))), 
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[2]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[3]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[4]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[5]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[6]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[7]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[8]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[9]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[10]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[11]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[12]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[13]].usd * 1000000))),
-        new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[14]].usd * 1000000)))
-    ];
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[0]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[1]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[2]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[3]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[4]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[5]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[6]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[7]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[8]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[9]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[10]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[11]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[12]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[13]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_2[14]].usd * 1000000))),
+            ];
+            const var3 = [
+                new Uint8Array(Buffer.from('update_slot_3')),
 
-}
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[0]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[1]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[2]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[3]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[4]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[5]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[6]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[7]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[8]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[9]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[10]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[11]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[12]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[13]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_3[14]].usd * 1000000))),
+            ];
+            const var4 = [
+                new Uint8Array(Buffer.from('update_slot_4')),
 
-
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[0]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[1]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[2]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[3]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[4]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[5]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[6]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[7]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[8]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[9]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[10]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[11]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[12]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[13]].usd * 1000000))),
+                new Uint8Array(Buffer.from(Math.trunc(data[update_slot_4[14]].usd * 1000000))),
+            ];
+        };
     }, 4000); // Tiempo que se tarda en actualizar el server
 };
 
